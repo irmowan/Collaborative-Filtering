b70c7c5912106f2f4fad03eae8fbd8d695e43260
irmowan <irmowan@gmail.com>
2016-06-19 11:41:45 +0800

    Update predict functions
    增加user归一化矩阵的测试

--- predict.py
+++ predict.py
@@ -2,18 +2,19 @@ import var
 import numpy as np
 
 def predict_baseline(user, item):
+    '''baseline'''
     prediction = var.item_mean[item] + var.user_mean[user] - var.all_mean
     return prediction
 
-def predict_itemCF(user, item, k=100):
-    '''item-item协同过滤算法,预测rating'''
+def predict_itemCF(user, item):
+    '''item-item协同过滤算法'''
     nzero = var.ratings[user].nonzero()[0]
     prediction = var.ratings[user, nzero].dot(var.item_similarity[item, nzero])\
                 / sum(var.item_similarity[item, nzero])
     return prediction
 
-def predict_userCF(user, item, k=100):
-    '''user-user协同过滤算法,预测rating'''
+def predict_userCF(user, item):
+    '''user-user协同过滤算法'''
     nzero = var.ratings[:,item].nonzero()[0]
     prediction = (var.ratings[nzero, item]).dot(var.user_similarity[user, nzero])\
                 / sum(var.user_similarity[user, nzero])
@@ -22,25 +23,24 @@ def predict_userCF(user, item, k=100):
         prediction = baseline[user]
     return prediction
 
-def predict_itemCF_baseline(user, item, k=100):
-    '''结合baseline的item-item CF算法,预测rating'''
+def predict_itemCF_baseline(user, item):
+    '''结合baseline的item-item CF算法'''
     nzero = var.ratings[user].nonzero()[0]
     baseline = var.item_mean + var.user_mean[user] - var.all_mean
     prediction = (var.ratings[user, nzero] - baseline[nzero]).dot(var.item_similarity[item, nzero])\
                 / sum(var.item_similarity[item, nzero]) + baseline[item]
     return prediction 
 
-def predict_userCF_baseline(user, item, k=100):
+def predict_userCF_baseline(user, item):
     '''结合baseline的user-user协同过滤算法,预测rating'''
     nzero = var.ratings[:,item].nonzero()[0]
     baseline = var.user_mean + var.item_mean[item] - var.all_mean
     prediction = (var.ratings[nzero, item] - baseline[nzero]).dot(var.user_similarity[user, nzero])\
                 / sum(var.user_similarity[user, nzero]) + baseline[user]
-    if np.isnan(prediction):
-        prediction = baseline[user]
+    if np.isnan(prediction): prediction = baseline[user]
     return prediction
 
-def predict_itemCF_bias(user, item, k=100):
+def predict_itemCF_bias(user, item):
     '''结合baseline的item-item CF算法,预测rating'''
     nzero = var.ratings[user].nonzero()[0]
     baseline = var.item_mean + var.user_mean[user] - var.all_mean
@@ -68,28 +68,39 @@ def predict_topkCF_user(user, item, k=30):
     baseline = var.user_mean + var.item_mean[item] - var.all_mean
     prediction = (var.ratings[choice, item] - baseline[choice]).dot(var.user_similarity[user, choice])\
                 / sum(var.user_similarity[user, choice]) + baseline[user]
-    if np.isnan(prediction):
-        prediction = baseline[user]
-    if prediction > 5: prediction = 5
-    if prediction < 1: prediction = 1
-    return prediction
-
-def predict_blend(user, item, k1=20, k2=30, alpha=0.6):
-    '''融合算法，融合item-itemCF和user-userCF,预测rating'''
-    prediction1 = predict_topkCF_item(user, item, k1)
-    prediction2 = predict_topkCF_user(user, item, k2)
-    prediction = alpha * prediction1 + (1-alpha) * prediction2
+    if np.isnan(prediction): prediction = baseline[user]
     if prediction > 5: prediction = 5
     if prediction < 1: prediction = 1
     return prediction
 
 def predict_normCF_item(user, item, k=20):
-    '''在topK的基础上采用归一化矩阵'''
+    '''在topK的基础上对item采用归一化的相似度矩阵'''
     nzero = var.ratings[user].nonzero()[0]
     baseline = var.item_mean + var.user_mean[user] - var.all_mean
     choice = nzero[var.item_similarity_norm[item, nzero].argsort()[::-1][:k]]
     prediction = (var.ratings[user, choice] - baseline[choice]).dot(var.item_similarity_norm[item, choice])\
                 / sum(var.item_similarity_norm[item, choice]) + baseline[item]
+    if prediction > 5: prediction = 5
+    if prediction < 1: prediction = 1
+    return prediction
+
+def predict_normCF_user(user, item, k=30):
+    '''在topK的基础上对user采用归一化的相似度矩阵'''
+    nzero = var.ratings[:,item].nonzero()[0]
+    choice = nzero[var.user_similarity_norm[user, nzero].argsort()[::-1][:k]]
+    baseline = var.user_mean + var.item_mean[item] - var.all_mean
+    prediction = (var.ratings[choice, item] - baseline[choice]).dot(var.user_similarity_norm[user, choice])\
+                / sum(var.user_similarity_norm[user, choice]) + baseline[user]
+    if np.isnan(prediction): prediction = baseline[user]
+    if prediction > 5: prediction = 5
+    if prediction < 1: prediction = 1
+    return prediction
+
+def predict_blend(user, item, k1=20, k2=30, alpha=0.6):
+    '''融合模型'''
+    prediction1 = predict_topkCF_item(user, item, k1)
+    prediction2 = predict_topkCF_user(user, item, k2)
+    prediction = alpha * prediction1 + (1-alpha) * prediction2
     if prediction > 5: prediction = 5
     if prediction < 1: prediction = 1
     return prediction
